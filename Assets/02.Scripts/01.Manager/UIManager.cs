4be602436eef65892266eff37430fdbb30cfec6e
안우재 <ays97123@gmail.com>
2026-08-05 16:56:47 +0900

    feat: UIManager Extension 추가

--- Assets/02.Scripts/01.Manager/UIManager.cs
+++ Assets/02.Scripts/01.Manager/UIManager.cs
@@ -1,16 +1,14 @@
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] Canvas Canvas_Main;
+    [SerializeField] Canvas Canvas_Popup;
+    [SerializeField] Canvas Canvas_VeryFront;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public static UIManager Instance { get; set; }
+
+    private Dictionary<UIType, UIBase> _createdUIDic = new Dictionary<UIType, UIBase>();
+    private HashSet<UIType> _openedUIDic = new HashSet<UIType>();
 }
