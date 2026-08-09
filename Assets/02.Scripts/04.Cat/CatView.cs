6f25f37df6347137ab20180784c37150d24bc9ce
안우재 <ays97123@gmail.com>
2026-08-09 12:27:25 +0900

    feat: 목표 건물 방향으로 고양이가 가도록 구현

--- Assets/02.Scripts/04.Cat/CatView.cs
+++ Assets/02.Scripts/04.Cat/CatView.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class CatView : MonoBehaviour
 {
@@ -16,12 +18,42 @@ public class CatView : MonoBehaviour
         if(_catViewModel == null)
             return;
 
-        // TODO(안우재/08.06) : 목표 방향으로 갈 수 있도록 rotation 부분 추가 필요, Right 이동이 앞으로 개념
-        transform.Translate(Vector3.right * _catViewModel.CatSpeed * Time.deltaTime);
+        // TODO(안우재/08.06) : 목표 방향으로 갈 수 있도록 rotation 부분 추가 필요
+        transform.Translate(Vector3.forward * _catViewModel.CatSpeed * Time.deltaTime);
     }
 
     public void InitCatView(CatViewModel catViewModel)
     {
         _catViewModel = catViewModel;
+
+        // TODO(안우재/08.09) : 테스트 초기화. 나중에 건물 짓기에서 건물 오브젝트 매니저 생길 시 
+        // 오브젝트 매니저에서 건물리스트 가져와서 처리 해야함(완전 Test코드)
+
+        GameObject[] objects = FindObjectsByType<GameObject>(FindObjectsSortMode.None);
+        List<Transform> testPositions = new();
+
+        foreach (GameObject obj in objects)
+        {
+            if (obj.name.Contains("BuildingTest"))
+            {
+                testPositions.Add(obj.transform);
+            }
+        }
+
+        if (testPositions.Count == 0)
+            return;
+
+        int randomIndex = GameUtil.Random.Next(0, testPositions.Count);
+        _targetPosition = testPositions[randomIndex];
+
+        //
+
+        if (_targetPosition == null)
+            return;
+
+        Vector3 direction = (_targetPosition.position - transform.position).normalized;
+        direction.y = 0f;
+        direction.z = 0f;
+        transform.rotation = Quaternion.LookRotation(direction);
     }
 }
