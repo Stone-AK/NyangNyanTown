acc8bd024933dee465fef5c711589431eea2be8c
안우재 <ays97123@gmail.com>
2026-08-14 01:50:03 +0900

    feat: 고양이 클릭 시 팝업, 도감 팝업 틀 작성

---
+++ Assets/02.Scripts/04.Cat/GameCatUI/CatEncyclopediaPopUp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CatEncyclopediaPopUp : MonoBehaviour
+{
+    private List<CatEncyclopediaViewModel> _catEncyclopediaList = new();
+
+    private void InitCatEncyclopedList()
+    {
+        if (GameManager.Instance.DataManager.TryGetDataTable<CatInfoData>(out var dataTable))
+        {
+            foreach (var data in dataTable)
+            {
+                CatEncyclopediaViewModel newCatData = new();
+                newCatData.CatInfoDataId = data.Key;
+                _catEncyclopediaList.Add(newCatData);
+            }
+        }
+    }
+
+}
--- Assets/02.Scripts/04.Cat/GameCatUI/OnClickCatInfoPopUp.cs
+++ Assets/02.Scripts/04.Cat/GameCatUI/OnClickCatInfoPopUp.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class CatEncyclopedia : MonoBehaviour
+public class OnClickCatInfoPopUp : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
