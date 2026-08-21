290ce5323d010307468551368b6ff21e438a2ba8
한재덕 <gkswoejr6@gmail.com>
2026-08-21 14:48:52 +0900

    feat: 메인UI 변경값 표시기능, 빌드UI 통합

--- Assets/02.Scripts/05.Builder/BuildUIView.cs
+++ Assets/02.Scripts/05.Builder/BuildUIView.cs
@@ -2,12 +2,10 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class BuildUIView : MonoBehaviour
+public class BuildUIView : BaseUI
 {
-    [SerializeField] Button Button_Build;
     [SerializeField] Transform _layoutGroup;
     [SerializeField] BuildingSlotItemView _slotPrefab;
-    [SerializeField] private GameObject _buildPanel;
 
     private BuildUIViewModel _viewModel;
     
@@ -15,31 +13,11 @@ public class BuildUIView : MonoBehaviour
     {
         _viewModel = new BuildUIViewModel();//TODO : 매니저에서 생성하도록 변경
         CreateSlots();
-        _buildPanel.SetActive(false);
-        BindOnClickButtonEvent(OnClickBuildButton);
 
     }
-    public void BindOnClickButtonEvent(Action onClickCallback)
-    {
-        if (Button_Build == null) return;
-
-        Button_Build.onClick.AddListener(new UnityEngine.Events.UnityAction(onClickCallback));
-
-    }
-    public void UnBindOnClickButtonEvent(Action onClickCallback)
-    {
-        if (Button_Build == null) return;
-
-        Button_Build.onClick.RemoveListener(new UnityEngine.Events.UnityAction(onClickCallback));
-    }
-    private void OnClickBuildButton() 
-    {
-        _buildPanel.SetActive(!_buildPanel.activeSelf);
-    }
-    private void OnDestroy()
-    {
-        UnBindOnClickButtonEvent(OnClickBuildButton);
-    }
+   
+   
+   
     private void CreateSlots() 
     {
         foreach (var slotViewModel in _viewModel._itemSlots)
